5bfa9867bfc192b60a88aec6fd9efa41df649c44
sebastianvicog <sebastianvicog@gmail.com>
2024-02-21 19:59:17 +0100

    File modified

--- src/main/java/es/ujaen/ssccdd/Main.java
+++ src/main/java/es/ujaen/ssccdd/Main.java
@@ -10,47 +10,46 @@ import java.util.concurrent.TimeUnit;
  *  Main class of the example
  */
 public class Main {
-    /**
-     * Main method of the example
-     * @param args
-     */
-    public static void main(String[] args) {
+
 
 //[1.] To create and to execute threads
 /*
+    public static void main(String[] args) {
         //Launch 10 threads that make the operation with a different number
-        for (int i=1; i<=10; i++){
-            Calculator calculator=new Calculator(i);
-            Thread thread=new Thread(calculator);
+        for (int i = 1; i <= 10; i++) {
+            Calculator calculator = new Calculator(i);
+            Thread thread = new Thread(calculator);
             thread.start();
         }
     }
-}
 */
+
+
 
 //[2.] To get thread information
 /*
+    public static void main(String[] args) {
         // Thread priority information
-        System.out.printf("Minimum Priority: %s\n",Thread.MIN_PRIORITY);
-        System.out.printf("Normal Priority: %s\n",Thread.NORM_PRIORITY);
-        System.out.printf("Maximun Priority: %s\n",Thread.MAX_PRIORITY);
+        System.out.printf("Minimum Priority: %s\n", Thread.MIN_PRIORITY);
+        System.out.printf("Normal Priority: %s\n", Thread.NORM_PRIORITY);
+        System.out.printf("Maximun Priority: %s\n", Thread.MAX_PRIORITY);
 
         Thread threads[];
         Thread.State status[];
 
         // Launch 10 threads to do the operation, 5 with the max
         // priority, 5 with the min
-        threads=new Thread[10];
-        status=new Thread.State[10];
+        threads = new Thread[10];
+        status = new Thread.State[10];
 
-        for (int i=0; i<10; i++){
-            threads[i]=new Thread(new Calculator(i));
-            if ((i%2)==0){
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new Calculator(i));
+            if ((i % 2) == 0) {
                 threads[i].setPriority(Thread.MAX_PRIORITY);
             } else {
                 threads[i].setPriority(Thread.MIN_PRIORITY);
             }
-            threads[i].setName("Thread "+i);
+            threads[i].setName("Thread " + i);
         }
 
         try (FileWriter file = new FileWriter(".\\data\\log.txt"); PrintWriter pw = new PrintWriter(file);) {
@@ -83,23 +82,26 @@ public class Main {
 
     }
 
-        /**
-         *  This method writes the state of a thread in a file
-         * @param pw : PrintWriter to write the data
-         * @param thread : Thread whose information will be written
-         * @param state : Old state of the thread
-         */
-/*
-        private static void writeThreadInfo(PrintWriter pw, Thread thread, Thread.State state) {
-            pw.printf("Main : Id %d - %s\n",thread.getId(),thread.getName());
-            pw.printf("Main : Priority: %d\n",thread.getPriority());
-            pw.printf("Main : Old State: %s\n",state);
-            pw.printf("Main : New State: %s\n",thread.getState());
-            pw.printf("Main : ************************************\n");
-        }
+     //
+     // This method writes the state of a thread in a file
+     //
+     // @param pw     : PrintWriter to write the data
+     // @param thread : Thread whose information will be written
+     // @param state  : Old state of the thread
+     //
+    private static void writeThreadInfo(PrintWriter pw, Thread thread, Thread.State state) {
+        pw.printf("Main : Id %d - %s\n", thread.getId(), thread.getName());
+        pw.printf("Main : Priority: %d\n", thread.getPriority());
+        pw.printf("Main : Old State: %s\n", state);
+        pw.printf("Main : New State: %s\n", thread.getState());
+        pw.printf("Main : ************************************\n");
+    }
 */
 
+
+
 //[3.] To interrupt the execution of thread
+    public static void main(String[] args) {
         // Launch the prime numbers generator
         Thread task=new PrimeGenerator();
         task.start();
@@ -113,10 +115,6 @@ public class Main {
 
         // Interrupt the prime number generator
         task.interrupt();
-
-
     }
 
 }
-
-
